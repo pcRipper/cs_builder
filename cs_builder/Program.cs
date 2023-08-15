f18514bca9c1db5b8774adc35b5f8edb09c2c3e1
pcRipper <maximuspro100@gmail.com>
2023-08-15 19:31:11 +0300

    mc

---
+++ cs_builder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
